1723bb7a408bb813e5586c7006ad5afd0e9f746d
Paras Surti <paras16surti@gmail.com>
2026-09-07 15:42:47 +0530

    Feature : Added a Play Again Feature

--- src/Game.java
+++ src/Game.java
@@ -1,27 +1,40 @@
-
 import java.util.Scanner;
 import java.util.random.RandomGenerator;
 
-public class Main
+public class Game
 {
 
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        System.out.println("!! WELCOME TO NUMBER GUESSING GAME !!");
+        while(true)
+        {
+            System.out.println("!! WELCOME TO NUMBER GUESSING GAME !!");
+            int target = RandomGenerator.getDefault().nextInt(101);
 
-        int target = RandomGenerator.getDefault().nextInt(101);
+            int totalAttempts = 0;
+            Difficulty difficulty = new Difficulty();
+            while(true)
+            {
+                System.out.print("Select the Difficulty : ");
+                String mode = input.next();
+                totalAttempts = difficulty.totalAttempt(mode);
+                if(totalAttempts != 0 )
+                {
+                    break;
+                }
+            }
+            System.out.println("You Have Total " + totalAttempts + " Attempt");
+            System.out.println("Enter Your Guess (0 - 100) ");
+            Attempts attempts = new Attempts(target, totalAttempts);
 
-        System.out.print("Select the Difficulty : ");
-        String mode = input.next();
-
-        Difficulty difficulty = new Difficulty();
-        int totalAttempts = difficulty.totalAttempt(mode);
-
-
-        System.out.println("You Have Total " + totalAttempts + " Attempt");
-        System.out.println("Enter Your Guess (0 - 100) ");
-        Attempts attempts = new Attempts(target, totalAttempts);
-
+            System.out.print("Do You Wanna Play Again (Y/N) :");
+            String ch = input.next();
+            if(ch.equalsIgnoreCase("N"))
+            {
+                System.out.println("You have successfully exited the game");
+                break;
+            }
+        }
     }
 }
