59f00eac8bec8ae5b5c8f11d104f74de6c3a015c
Paras Surti <paras16surti@gmail.com>
2026-09-07 14:45:40 +0530

    Determines the allowed attempt based on the difficulty selected by user

--- src/Difficulty.java
+++ src/Difficulty.java
@@ -2,9 +2,15 @@ public class Difficulty
 {
     public static int totalAttempt(String mode)
     {
-        if(mode.equals("easy")) return 10;
-        if(mode.equals("medium")) return 7;
-        else return 5;
-
+        String str = mode.toLowerCase();
+        if(str.equals("easy")) return 10;
+        if(str.equals("medium")) return 7;
+        if(str.equals("hard")) return 5;
+        else
+        {
+            System.out.println("Enter the Difficulty properly ");
+            System.out.println("'easy', 'medium', 'hard' ");
+        }
+        return 0;
     }
 }
