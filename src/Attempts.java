dae8b03774f308e725d6dfb6e77f1bc7f4d46aba
Paras Surti <paras16surti@gmail.com>
2026-09-07 21:50:10 +0530

    This Class Contains Hint Feature

--- src/Attempts.java
+++ src/Attempts.java
@@ -2,18 +2,27 @@ import java.util.Scanner;
 
 public class Attempts
 {
-    public Attempts(int target, int totalAttempt)
+    public void play(int target, int totalAttempt)
     {
         Scanner input = new Scanner(System.in);
         int attempts = 0;
+        guessingMethod(input,attempts, target,totalAttempt);
+
+    }
+    public void guessingMethod(Scanner input, int attempts, int target, int totalAttempt)
+    {
+        Scoring score = new Scoring();
+        int diff = 0;
         while(true)
         {
+            System.out.print("Guess : " );
             int guess = input.nextInt();
-            attempts++;
+            Difference difference = new Difference();
             if(guess == target)
             {
                 System.out.println("Correct! You guessed the number!" );
                 System.out.println("Total Attempt : " + attempts );
+                score.scoreCard(attempts);
                 break;
             }
             if(attempts == totalAttempt)
@@ -23,10 +32,23 @@ public class Attempts
                 System.out.println("The Number was " + target);
                 break;
             }
-            if(guess > target) System.out.println("Too High! ");
-            else System.out.println("Too low! ");
-            System.out.println("Your Total Attempts : " + attempts);
-
+            if(guess > target && (guess >= 0 && guess <= 100))
+            {
+                System.out.println("Too High! ");
+                diff = Math.abs(target - guess);
+                difference.difference(diff);
+                attempts++;
+                System.out.println("Your Total Attempts : " + attempts);
+            }
+            else if(guess < target && (guess >= 0 && guess <= 100))
+            {
+                System.out.println("Too low! ");
+                diff = Math.abs(target - guess);
+                difference.difference(diff);
+                attempts++;
+                System.out.println("Your Total Attempts : " + attempts);
+            }
+            else System.out.println("Invalid Number (Out Of Range)\nEnter the Guess Again (0-100) : ");
         }
     }
 }
