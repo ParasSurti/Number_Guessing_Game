5b211702b0f139f8ea63335ac7892c694aaf63ca
Paras Surti <paras16surti@gmail.com>
2026-09-07 14:43:18 +0530

    Runs the guessing game loop

---
+++ src/Attempts.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Attempts
+{
+    public Attempts(int target, int totalAttempt)
+    {
+        Scanner input = new Scanner(System.in);
+        int attempts = 0;
+        while(true)
+        {
+            int guess = input.nextInt();
+            attempts++;
+            if(guess == target)
+            {
+                System.out.println("Correct! You guessed the number!" );
+                System.out.println("Total Attempt : " + attempts );
+                break;
+            }
+            if(attempts == totalAttempt)
+            {
+                System.out.println("!! Game Over !! ");
+                System.out.println("You Have Used all the Attempts !! ");
+                System.out.println("The Number was " + target);
+                break;
+            }
+            if(guess > target) System.out.println("Too High! ");
+            if(guess < target) System.out.println("Too low! ");
+            System.out.println("Your Total Attempts : " + attempts);
+
+        }
+    }
+}
